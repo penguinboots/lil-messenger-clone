d228adbc04279220bcd5e29e7310d057dd641dde
penguinboots <sabrina.ynw@gmail.com>
2023-07-07 19:18:01 -0700

    build userbox component

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { User } from ".prisma/client";
+import Avatar from "@/app/components/Avatar";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
@@ -15,21 +16,59 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
 
   const handleClick = useCallback(() => {
     setIsLoading(true);
-    axios.post('/api/conversations', {
-      userId: data.id
-    })
-    .then((data) => {
-      router.push(`/conversations/${data.data.id}`)
-    })
-    .finally(() => {
-      setIsLoading(false)
-    })
-  }, [data, router])
+    axios
+      .post("/api/conversations", {
+        userId: data.id,
+      })
+      .then((data) => {
+        router.push(`/conversations/${data.data.id}`);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [data, router]);
 
   return (
-  <div>
-
-  </div>
+    <div
+      onClick={handleClick}
+      className="
+        w-full
+        relative
+        flex
+        items-center
+        space-x-3
+        bg-white
+        p-3
+        hover:bg-neutral-100
+        rounded-lg
+        transition
+        cursor-pointer
+      "
+    >
+      <Avatar user={data} />
+      <div className="min-w-0 flex-1">
+        <div className="focus:outline-none">
+          <div
+            className="
+            flex
+            justify-between
+            items-center
+            mb-1
+          "
+          >
+            <p
+              className="
+              text-sm
+              font-medium
+              text-gray-900
+            "
+            >
+              {data.name}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
