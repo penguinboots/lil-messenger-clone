52058ca360f26146e9f94b355e0b2f3fdee81db0
penguinboots <sabrina.ynw@gmail.com>
2023-07-11 11:42:12 -0700

    build settings modal content

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 
 import Avatar from "../Avatar";
 import DesktopItem from "./DesktopItem";
+import SettingsModal from "./SettingsModal";
 
 interface DesktopSidebarProps {
   currentUser: User;
@@ -16,8 +17,14 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div
-      className="
+    <>
+      <SettingsModal
+        currentUser={currentUser}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+      <div
+        className="
       hidden
       lg:fixed
       lg:inset-y-0
@@ -32,57 +39,58 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
       lg:flex
       lg:flex-col
       justify-between"
-    >
-      <nav
-        className="
+      >
+        <nav
+          className="
         mt-4
         flex
         flex-col
         justify-between
       "
-      >
-        <ul
-          role="list"
-          className="
+        >
+          <ul
+            role="list"
+            className="
             flex
             flex-col
             items-center
             space-y-1
           "
-        >
-          {routes.map((item) => (
-            <DesktopItem
-              key={item.label}
-              href={item.href}
-              label={item.label}
-              icon={item.icon}
-              active={item.active}
-              onClick={item.onClick}
-            />
-          ))}
-        </ul>
-      </nav>
-      <nav
-        className="
+          >
+            {routes.map((item) => (
+              <DesktopItem
+                key={item.label}
+                href={item.href}
+                label={item.label}
+                icon={item.icon}
+                active={item.active}
+                onClick={item.onClick}
+              />
+            ))}
+          </ul>
+        </nav>
+        <nav
+          className="
           mt-4
           flex
           flex-col
           justify-between
           items-center  
         "
-      >
-        <div
-          onClick={() => setIsOpen(true)}
-          className="
-          cursor-pointer
-          hover:opacity-75
-          transition
-          "
         >
-          <Avatar user={currentUser}/>
-        </div>
-      </nav>
-    </div>
+          <div
+            onClick={() => setIsOpen(true)}
+            className="
+            cursor-pointer
+            hover:opacity-75
+            transition
+          "
+          >
+            <Avatar user={currentUser} />
+          </div>
+        </nav>
+      </div>
+    </>
   );
 };
 
--- app/components/sidebar/SettingsModal.tsx
+++ app/components/sidebar/SettingsModal.tsx
@@ -6,6 +6,11 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
+import Modal from "../Modal";
+import Input from "../inputs/Input";
+import Image from "next/image";
+import { CldUploadButton } from "next-cloudinary";
+import Button from "../Button";
 
 interface SettingsModalProps {
   isOpen?: boolean;
@@ -44,7 +49,6 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
-
     axios
       .post("/api/settings", data)
       .then(() => {
@@ -55,7 +59,65 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
       .finally(() => setIsLoading(false));
   };
 
-  return <div>SettingsModal</div>;
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <form onSubmit={() => handleSubmit(onSubmit)}>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Profile
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Edit your public profile.
+            </p>
+            <div className="mt-10 flex flex-col gap-y-8">
+              <Input
+                disabled={isLoading}
+                label="Name"
+                id="name"
+                errors={errors}
+                required
+                register={register}
+              />
+              <div>
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                  Photo
+                </label>
+                <div className="mt-2 flex items-center gap-x-3">
+                  <Image
+                    width="48"
+                    height="48"
+                    className="rounded-full"
+                    src={
+                      image || currentUser?.image || "/images/placeholder.png"
+                    }
+                    alt="avatar"
+                  />
+                  <CldUploadButton
+                    options={{ maxFiles: 1 }}
+                    onUpload={handleUpload}
+                    uploadPreset="ihcb43lo"
+                  >
+                    <Button disabled={isLoading} secondary type="button">
+                      Change
+                    </Button>
+                  </CldUploadButton>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <Button disabled={isLoading} secondary onClick={onClose}>
+              Cancel
+            </Button>
+            <Button disabled={isLoading} type="submit">
+              Save
+            </Button>
+          </div>
+        </div>
+      </form>
+    </Modal>
+  );
 };
 
 export default SettingsModal;
